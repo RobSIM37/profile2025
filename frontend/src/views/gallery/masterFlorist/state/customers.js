7dc3f38e7c1d3d995a761ff3dafa2efd3baac664
Robert Curran Lewis, IV <robsim37@gmail.com>
2025-10-05 00:53:29 -0400

    upped foot traffic

--- frontend/src/views/gallery/masterFlorist/state/customers.js
+++ frontend/src/views/gallery/masterFlorist/state/customers.js
@@ -42,9 +42,9 @@ const FOOT_TRAFFIC_INTERVALS = {
 };
 
 const FOOT_TRAFFIC_SPAWN_CHANCE = {
-  relaxed: 0.04,
-  steady: 0.06,
-  brisk: 0.08,
+  relaxed: 0.10,
+  steady: 0.14,
+  brisk: 0.18,
 };
 
 const FOOT_TRAFFIC_PATIENCE_MULTIPLIERS = Object.freeze({
